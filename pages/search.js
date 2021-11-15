fa4085ee8d02df5fb27596c6856e1806fe8b6c01
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-11-14 19:51:52 -0500

    finished input locations; set up saved places; did some styles on the search.js page

--- pages/search.js
+++ pages/search.js
@@ -16,12 +16,16 @@ const Search = () => {
                     <FromHereToThereIconSquare src='https://img.icons8.com/windows/50/000000/square-full.png' />
                 </FromHereToThereIcons>
                 <InputField>
-                    <Input placeholder='Pickup Location'/>
-                    <Input  placeholder='Where To?'/>
+                    <Input placeholder='Pickup Location' />
+                    <Input placeholder='Where To?' />
                 </InputField>
+                <PlusIconButton src='https://img.icons8.com/ios/50/000000/plus-math.png' />
             </InputContainer>
             {/* saved places section */}
-
+            <SavedPlacesContainer>
+                <StarIcon src='https://img.icons8.com/ios-filled/50/ffffff/star--v1.png' />
+                Saved Places
+            </SavedPlacesContainer>
             {/* confrim locations button */}
 
         </Wrapper>
@@ -40,19 +44,19 @@ const BackButton = tw.img`
 h-12
 `
 const InputContainer = tw.div`
-flex bg-white
+flex bg-white items-center px-4 mb-2
 `
 const FromHereToThereIcons = tw.div`
-flex flex-col w-10 
+flex flex-col w-10 mr-2 items-center
 `
 const FromHereToThereIconCircle = tw.img`
-
+h-2.5
 `
 const FromHereToThereIconLine = tw.img`
-
+h-10
 `
 const FromHereToThereIconSquare = tw.img`
-
+h-3
 `
 const InputField = tw.div`
 flex flex-col flex-1
@@ -60,4 +64,12 @@ flex flex-col flex-1
 const Input = tw.input`
 h-10 bg-gray-200 my-2 rounded-lg p-2 outline-none border-none
 `
-
+const PlusIconButton = tw.img`
+w-10 h-10 bg-gray-200 rounded-full ml-3
+`
+const SavedPlacesContainer = tw.div`
+flex items-center px-4 py-4 bg-white
+`
+const StarIcon = tw.img`
+bg-gray-400 h-10 w-10 p-2 mr-2 rounded-full
+`
