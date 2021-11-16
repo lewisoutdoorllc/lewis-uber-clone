3298d3fe999e3639be15854392cd83e1f2c0dd26
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-11-15 22:56:05 -0500

    set up dropoff and pickup locations in confirm.js; added a default marker on mapbox

--- pages/componets/Map.js
+++ pages/componets/Map.js
@@ -13,8 +13,16 @@ const Map = () => {
             center: [-99.29011, 39.39172],
             zoom: 3
         })
+
+        addToMap(map)
+
     })
 
+    const addToMap = (map) => {
+        const marker1 = new mapboxgl.Marker()
+            .setLngLat([-99.29011, 39.39172])
+            .addTo(map);
+    }
 
     return (
         <Wrapper id='map'>
--- pages/confirm.js
+++ pages/confirm.js
@@ -1,16 +1,53 @@
-import React from 'react'
+import { useEffect } from 'react'
 import tw from 'tailwind-styled-components'
 import Map from './componets/Map'
 
 
 const Confirm = () => {
+    // set up mapbox to get coordinates for trip
+    const getPickupCoords = () => {
+        const pickup = 'Orlando, FL'
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?` +
+            new URLSearchParams({
+                access_token: 'pk.eyJ1IjoibGV3aXNvdXRkb29yIiwiYSI6ImNrdnprNjl1cTEyeXoyd3BhbGN3M2ZlYzgifQ.Gc42G5eAvQUvUfSKepFipw',
+                limit: 1
+            })
+        )
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.features[0].center)
+            })
+    }
+
+    const getDropoffCoords = () => {
+        const dropoff = 'Oviedo, FL'
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropoff}.json?` +
+            new URLSearchParams({
+                access_token: 'pk.eyJ1IjoibGV3aXNvdXRkb29yIiwiYSI6ImNrdnprNjl1cTEyeXoyd3BhbGN3M2ZlYzgifQ.Gc42G5eAvQUvUfSKepFipw',
+                limit: 1
+            })
+        )
+            .then(res => res.json())
+            .then(data => {
+                console.log(dropoff)
+                console.log(data.features[0].center)
+            })
+    }
+
+    useEffect(() => {
+        getPickupCoords()
+        getDropoffCoords()
+    }, [])
+
+
+
     return (
         <Wrapper>
             <Map />
             <ConfirmRideTypeContainer>
-{/* select the type of ride section */}
+                {/* select the type of ride section */}
 
-{/* confirm uber button */}
+                {/* confirm uber button */}
 
             </ConfirmRideTypeContainer>
         </Wrapper>
